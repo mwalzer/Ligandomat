d92e482d3f75cad137b04fddb78d61e821b8b9a5
linusb <backert@informatik.uni-tuebingen.de>
2014-02-13 15:08:29 +0100

    removed Page file need

--- ligandomat/scripts/initializedb.py
+++ ligandomat/scripts/initializedb.py
@@ -33,6 +33,6 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = Page('FrontPage', 'Welcome to Ligandomat')
-        DBSession.add(model)
+    #with transaction.manager:
+    #    model = Page('FrontPage', 'Welcome to Ligandomat')
+    #    DBSession.add(model)
